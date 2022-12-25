ecd56afac1750e85902d0c871961761b0bcbc7f2
Advaith <advaith@chainist.com>
2022-12-25 04:17:48 -0500

    --gas-report for all contracts

---
+++ test/optimizations/MsgSenderPatterns.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {Test} from "forge-std/Test.sol";
+
+contract MsgSenderPatterns is Test {
+    Contract0 c0;
+    Contract1 c1;
+    Contract2 c2;
+    Contract3 c3;
+
+    function setUp() public {
+        c0 = new Contract0();
+        c1 = new Contract1();
+        c2 = new Contract2();
+        c3 = new Contract3();
+    }
+
+    function testGas() public {
+        c0.addressInternalBalance();
+        c1.assemblyInternalBalance();
+        c2.addressExternalBalance(address(this));
+        c3.assemblyExternalBalance(address(this));
+    }
+}
+
+contract Contract0 {
+    function addressInternalBalance() public returns (uint256) {
+        return address(this).balance;
+    }
+}
+
+contract Contract1 {
+    function assemblyInternalBalance() public returns (uint256) {
+        assembly {
+            let c := selfbalance()
+            mstore(0x00, c)
+            return(0x00, 0x20)
+        }
+    }
+}
+
+contract Contract2 {
+    function addressExternalBalance(address addr) public {
+        uint256 bal = address(addr).balance;
+        bal++;
+    }
+}
+
+contract Contract3 {
+    function assemblyExternalBalance(address addr) public {
+        uint256 bal;
+        assembly {
+            bal := balance(addr)
+        }
+        bal++;
+    }
+}
