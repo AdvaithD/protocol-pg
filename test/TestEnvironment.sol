a104dfbc196386e3973fcfa8de88461dc6e90d02
Advaith <advaith@chainist.com>
2022-12-26 21:09:43 -0500

    feat: implement gas benchmark test b/w oz clones and immutable args clones

--- test/TestEnvironment.sol
+++ test/TestEnvironment.sol
@@ -4,9 +4,15 @@ pragma solidity 0.8.15;
 import "forge-std/Test.sol";
 
 import {EIP1167Factory} from "../src/patterns/EIP1167Factory.sol";
+import {ImmutableArgsCloneFactory} from "../src/patterns/immutable-args/ImmutableArgsCloneFactory.sol";
 
 contract TestEnvironment is Test {
+    /// @notice EIP1167 factory
     EIP1167Factory public cloneFactory;
+
+    /// @notice Clones w/ immutable args factory
+    ImmutableArgsCloneFactory public immutableArgsCloneFactory;
+
     uint256 public constant privateKey = 11111111111;
 
     /// @notice deployer nonce (test setup deplyoer)
@@ -19,5 +25,7 @@ contract TestEnvironment is Test {
         owner = vm.addr(privateKey);
 
         cloneFactory = new EIP1167Factory();
+
+        immutableArgsCloneFactory = new ImmutableArgsCloneFactory();
     }
 }
--- test/patterns/ClonesTest.t.sol
+++ test/patterns/ClonesTest.t.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.15;
 
 import {console2} from "forge-std/console2.sol";
+import "../../src/patterns/immutable-args/CloneWithImmutableArgs.sol";
 import "../TestEnvironment.sol";
 
 contract ClonesTest is TestEnvironment {
@@ -11,5 +12,9 @@ contract ClonesTest is TestEnvironment {
         address clone = cloneFactory.createClone(address(0), 1, 2);
 
         console2.log("clone: %s", clone);
+    }
+
+    function testGas_cloneWithImmutableArgs() external {
+        Clone clone = immutableArgsCloneFactory.createClone(address(1), 1, 2);
     }
 }
