5e27064f8c240b48571b03de742eeaf12a579b44
Advaith <advaith@chainist.com>
2022-12-26 20:50:00 -0500

    feat: modify clone and factory, deploy impl inside factory deploy

--- src/patterns/Clone.sol
+++ src/patterns/Clone.sol
@@ -6,11 +6,17 @@ import {Reentrancy} from "../lib/Reentrancy.sol";
 /// @title Clone
 /// @notice Sample Clone contract that uses EIP1167 factory pattern
 contract Clone is Reentrancy {
+    /// @notice Factory that deploys clones (set only once)
+    address public immutable factory;
+
     /// @notice Wrapped ether address (example variable to show how initialize works)
     address internal wethAddress;
 
-    /// @notice Factory that deploys clones (set only once)
-    address public immutable factory;
+    /// @notice Another state variable
+    uint256 public uintOne;
+
+    /// @notice Another state variable
+    uint256 public uintTwo;
 
     /// @notice Used when initially deployed the implementation
     /// @param factory_ EIP1167Factory address
@@ -20,11 +26,15 @@ contract Clone is Reentrancy {
 
     /// @notice Initializes the clone
     /// @param _wethAddress Wrapped ether address
-    function initialize(address _wethAddress) external returns (address) {
+    /// @param _uintOne Storage variable param (as an example to generate salt)
+    /// @param _uintTwo Storage variable param (as an example to generate salt)
+    function initialize(address _wethAddress, uint256 _uintOne, uint256 _uintTwo) external returns (address) {
         // only factory can call this
         require(msg.sender == factory, "unauthorized caller");
 
         wethAddress = _wethAddress;
+        uintOne = _uintOne;
+        uintTwo = _uintTwo;
         return address(this);
     }
 }
--- src/patterns/EIP1167Factory.sol
+++ src/patterns/EIP1167Factory.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.15;
 
 import {Clones} from "../lib/Clones.sol";
+import {Clone} from "./Clone.sol";
 
 interface IEIP1167FactoryEvents {
     /// @notice New clone deployed
@@ -11,28 +12,33 @@ interface IEIP1167FactoryEvents {
 }
 
 interface IClone {
-    function initialize(address _wethAddress) external returns (address);
+    function initialize(address _wethAddress, uint256 _uintOne, uint256 _uintTwo) external returns (address);
 }
 
 contract EIP1167Factory is IEIP1167FactoryEvents {
     /// @notice implementation contract address
     address public immutable implementation;
 
-    constructor(address _implementation) {
-        implementation = _implementation;
+    /// @notice deploy the factory
+    /// and along with it, a new Clone and pass in factory address to the implementation
+    /// NOTE: We only need this deployed once
+    constructor() {
+        implementation = address(new Clone(address(this)));
     }
 
     /// @notice Deploys a new clone
-    /// @param TODO
-    function createClone() external returns (address clone) {
-        clone = Clones.cloneDeterministic(implementation, keccak256(abi.encode( /* */ )));
+    /// @param wethAddress for contract storage
+    /// @param _uintOne Storage variable param (as an example to generate salt)
+    /// @param _uintTwo Storage variable param (as an example to generate salt)
+    function createClone(address wethAddress, uint256 _uintOne, uint256 _uintTwo) external returns (address clone) {
+        clone = Clones.cloneDeterministic(implementation, keccak256(abi.encode(wethAddress, _uintOne, _uintTwo)));
 
-        IClone(clone).initialize( /* */ );
+        IClone(clone).initialize(wethAddress, _uintOne, _uintTwo);
 
         emit CloneDeployed(msg.sender, clone);
     }
 
-    function preictAddress(address implementation, bytes32 salt) external view returns (address) {
+    function predict(address implementation, bytes32 salt) external view returns (address) {
         return Clones.predictDeterministicAddress(implementation, salt, address(this));
     }
 }
